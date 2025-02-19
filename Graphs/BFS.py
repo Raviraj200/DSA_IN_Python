bbe41c63f236ac77215c4427bd17218f53d6fc45
Ravi raj Singh <99713321+ravirajji@users.noreply.github.com>
2025-02-19 18:42:31 +0530

    add BFS program

---
+++ Graphs/BFS.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def BFS(graph,start):
+    visited = set()
+    queue = deque([start])
+
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            print(node,end=" ")
+            visited.add(node)
+            queue.extend(graph[node]-visited)
+
+graph={
+    'A': {'B', 'C'},
+    'B': {'A', 'D', 'E'},
+    'C': {'A', 'F', 'G'},
+    'D': {'B'},
+    'E': {'B', 'H'},
+    'F': {'C'},
+    'G': {'C'},
+    'H': {'E'}
+}
+
+print("Starting node is A:")
+BFS(graph,'A')
