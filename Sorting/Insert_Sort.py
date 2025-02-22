5da221f68949f57e48cca156a3136b2a9aeebfab
Ravi raj Singh <99713321+ravirajji@users.noreply.github.com>
2025-02-22 18:32:57 +0530

    add Insert_Sort program

---
+++ Sorting/Insert_Sort.py
@@ -0,0 +1,12 @@
+def Insert_Sort(list):
+    for i in range(1,len(list)):
+        temp = list[i]
+        j=i-1
+        while j>=0 and temp<list[j]:
+            list[j+1] = list[j]
+            j-=1
+        list[j+1] = temp    
+
+list=[12, 11, 13, 5, 6]
+Insert_Sort(list)
+print(list)
